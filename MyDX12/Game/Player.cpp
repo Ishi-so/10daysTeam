11455d594cf6e177459677fa0df9653ce1cd5ee6
Ishi-so <k019g1077@g.neec.ac.jp>
2022-09-08 15:18:06 +0900

    状態作成

--- MyDX12/Game/Player.cpp
+++ MyDX12/Game/Player.cpp
@@ -61,8 +61,9 @@ void Player::Update()
 			velocity.x += 0.02f;
 		}
 	}
+
 	// 落下
-	//acc.y -= 0.0001f;
+	// acc.y -= 0.0001f;
 
 	// 左右加速度制御
 	if (abs(velocity.x) > 0.1f)
@@ -84,18 +85,38 @@ void Player::Update()
 	
 	// ---- 座標の更新 ----
 	velocity += acc;
-	position += velocity;
+	position += velocity * stateAcc;
 	object->position = position; // 座標の設定
 
 	// ---- 当たり判定を付随 ----
 	SetCollsion();
 
 	// ---- 当たり判定 ----
-	if (Math::HitCheck_AABB_Sphere(collBox, collSphere)) // box と sphereが当たったら
+	if (Math::HitCheck_AABB_Sphere(collBox, collSphere) && !invincible) // box と sphereが当たったら
 	{
-		// 赤色
-		boxObj->color = {1,0,0};
+		// 当たっていない状態だったら
+		if (!hitFlag)
+		{
+			// フラグをtrue
+			hitFlag = true;
+			// boxを赤色に設定
+			boxObj->color = { 1,0,0 };
+			// playerを青色に設定
+			object->color = {0,0,1};
+			// 無敵付与
+			state = State::invincible;
+		}
 	}
+	else 
+	{
+		//フラグをfalse
+		hitFlag = false;
+		// boxの色を戻す
+		boxObj->color = { 1,1,1 };
+	}
+
+	// 状態によって効果を付与
+	SetSkill();
 
 	// ---- 最終的な座標を設定　----
 	object->position = position; // 改めて設定(当たった時に押し出す処理を考慮)
@@ -103,6 +124,23 @@ void Player::Update()
 	// ---- objectの更新 ----
 	object->Update();
 	boxObj->Update();
+
+	// ---- カウンター ----
+	// 無敵状態かつ、カウントが最大までいったら
+	if (invincibleCnt >= INVINCIBLE_TIME && invincible) 
+	{
+		// 無敵解除
+		invincible = false;
+		// カウントを0クリア
+		invincibleCnt = 0;
+		// playerの色を戻す
+		object->color = { 1,1,1 };
+		// 状態を戻す
+		state = State::none;
+	}
+
+	// 無敵状態なら
+	if(invincible){ invincibleCnt++; } // 無敵時間をカウント
 }
 
 void Player::Draw()
@@ -127,4 +165,30 @@ void Player::SetCollsion()
 		boxObj->position.y - boxObj->scale.y,
 		boxObj->position.z - boxObj->scale.z,
 		1.0f);
+}
+
+void Player::SetSkill()
+{
+	// 状態によってそれぞれの効果を付与
+	if (state == State::none)
+	{
+		stateAcc = 1.0f;
+	}
+	else if (state == State::speedDown)
+	{
+		stateAcc = 0.5f;
+	}
+	else if (state == State::speedUp)
+	{
+		stateAcc = 1.5f;
+	}
+	else if (state == State::invincible)
+	{
+		invincible = true;
+		hitFlag = false;
+	}
+	else
+	{
+		stateAcc = 1.0f;
+	}
 }
--- MyDX12/Game/Player.h
+++ MyDX12/Game/Player.h
@@ -8,7 +8,17 @@ class Object3D;
 // プレイヤークラス
 class Player
 {
+private: // 構造体 or 列挙型 
+	// 状態
+	enum struct State:int
+	{
+		none = 0, // なし
+		speedDown, // 減速
+		speedUp, // 加速
+		invincible, // 無敵
+	};
 private: // メンバ定数
+	int INVINCIBLE_TIME = 60.0f; // 無敵時間
 public: // メンバ関数
 	Player() = default; // コンストラクタ
 	~Player(); // デストラクタ
@@ -18,16 +28,22 @@ public: // メンバ関数
 	void Draw(); // 描画
 
 	void SetCollsion(); // 当たり判定を設定
+	void SetSkill(); // 状態によってバフデバフを付与
 private: // メンバ変数
 	Object3D* object = nullptr; // ゲームオブジェクト
+	State state = State::none; // プレイヤーの状態
 	int hitPoint = 3; // 体力(仮)
 	Math::Vector3 position; // 座標
 	Math::Vector3 velocity; // 速度
 	Math::Vector3 acc; // 加速度
+	float stateAcc = 1.0f; // 状態によって決まる加減速値倍率
+	bool invincible = false; // 無敵フラグ
+	int invincibleCnt = 0; // 無敵時間のカウント
 
 	// 当たり判定確認用
-	Object3D* boxObj = nullptr;
-	Math::Sphere collSphere;
-	Math::AABB collBox;
+	Object3D* boxObj = nullptr; // box3Dオブジェクト
+	Math::Sphere collSphere; // 当たり判定用　球
+	Math::AABB collBox; // 当たり判定用　箱
+	bool hitFlag = false; // 当たり判定フラグ
 };
 
