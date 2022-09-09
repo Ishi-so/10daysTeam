e0aae1b82e18493a9b86cde18243c56586de8e6a
Ishi-so <k019g1077@g.neec.ac.jp>
2022-09-09 09:53:11 +0900

    namespace 削除

--- MyDX12/Game/ModelLoader.cpp
+++ MyDX12/Game/ModelLoader.cpp
@@ -1,30 +1,29 @@
 #include "ModelLoader.h"
 #include "../3D/Model.h"
-using namespace XIIlib;
 
-ModelLoader* XIIlib::ModelLoader::GetInstance()
+ModelLoader* ModelLoader::GetInstance()
 {
 	static ModelLoader instance;
 	return &instance;
 }
 
-void XIIlib::ModelLoader::Initialize()
+void ModelLoader::Initialize()
 {
 	models.resize(20);
 }
 
-void XIIlib::ModelLoader::Load(){
+void ModelLoader::Load(){
 	SetModel(MODEL_BLOCK, "box_v000");
 	SetModel(MODEL_ITEM, "box_v000");
 	SetModel(MODEL_PLAYER, "sphere");
 }
 
-Model* XIIlib::ModelLoader::GetModel(const ModelNum& modelNum) const
+Model* ModelLoader::GetModel(const ModelNum& modelNum) const
 {
 	return models[static_cast<int>(modelNum)].get();
 }
 
-void XIIlib::ModelLoader::SetModel(const ModelNum& modelNum,const std::string& modelName)
+void ModelLoader::SetModel(const ModelNum& modelNum,const std::string& modelName)
 {
 	std::unique_ptr<Model> uModel(Model::CreateFromOBJ(modelName));
 	models[static_cast<int>(modelNum)] = std::move(uModel);
