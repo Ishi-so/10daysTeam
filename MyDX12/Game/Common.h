1c078e6848148d73c4c3450ef6e287ee3efdb05f
R_Yoshioka <r.yoshioka1230@gmail.com>
2022-09-10 13:55:27 +0900

    コメント追加

--- MyDX12/Game/Common.h
+++ MyDX12/Game/Common.h
@@ -4,7 +4,7 @@
 namespace Common {
 	static float posStartX = -14.0f,posStartY = 0.0f;
 	static float posDistance = 2.0f;
-	static float constFrame = 1.0f / 60.0f;
+	static float constFrame = 1.0f / 60.0f;// 1フレーム分の少数点型
 
 	// クラスネームの取り出し関数
 	std::pair<std::string, std::string> SeparateFilePath(const std::string& path);
