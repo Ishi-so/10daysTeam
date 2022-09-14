5b3b7aeab5f6cb4029b0ca9e018811bf28a32f2e
Ishi-so <k019g1077@g.neec.ac.jp>
2022-09-14 20:47:26 +0900

    Player処理完成

--- MyDX12/GameScene/GameScene.h
+++ MyDX12/GameScene/GameScene.h
@@ -14,8 +14,8 @@ private:// 静的メンバ
 	static const int debugTextTexNumber = 0;
 	static const int debugJISTextTexNumber = 1;
 private: // メンバ定数
-	const float SHAKE_RAND_MIN = -0.5f;
-	const float SHAKE_RAND_MAX = 0.5f;
+	const float SHAKE_RAND_MIN = -0.3f;
+	const float SHAKE_RAND_MAX = 0.3f;
 	const int SHAKE_MAX_TIME = 20.0f;
 
 private:// メンバ
